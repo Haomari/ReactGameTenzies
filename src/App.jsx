46ac0ed80c3e4a1f92d6d6706da4d5eba95e8f6d
Haomari <tcurpal@gmail.com>
2023-06-29 19:07:53 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,29 @@ function App() {
 		<>
 			<Header />
 			<main className="page">
-			
+			<section className="page__main main">
+				<div className="main__container">
+					<div className="main__body">
+						<h2 className="main__title">
+							Tenzies
+						</h2>
+						<p className="main__text">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+						<div className="main__game game">
+							<button className="game__item"></button>
+							<button className="game__item"></button>
+							<button className="game__item"></button>
+							<button className="game__item"></button>
+							<button className="game__item"></button>
+							<button className="game__item"></button>
+							<button className="game__item"></button>
+							<button className="game__item"></button>
+							<button className="game__item"></button>
+							<button className="game__item"></button>
+						</div>
+						<button className="main__button">Roll</button>
+					</div>
+				</div>
+			</section>
 			</main>
 			<Footer />
 		</>
