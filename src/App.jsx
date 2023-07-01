98a19005026c45572d5e2fbd1da0a85f85a92703
Haomari <tcurpal@gmail.com>
2023-07-01 19:58:41 +0200

    html+scss done

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,29 @@ function App() {
   return (
 		<>
 			<Header />
-			<main className="page">
+			<main class="page">
 			<section className="page__main main">
 				<div className="main__container">
-					<div className="main__body">
-						<h2 className="main__title">
-							Tenzies
-						</h2>
-						<p className="main__text">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-						<div className="main__game game">
-							<button className="game__item"></button>
-							<button className="game__item"></button>
-							<button className="game__item"></button>
-							<button className="game__item"></button>
-							<button className="game__item"></button>
-							<button className="game__item"></button>
-							<button className="game__item"></button>
-							<button className="game__item"></button>
-							<button className="game__item"></button>
-							<button className="game__item"></button>
+					<div className="main__border">
+						<div className="main__body">
+							<h2 className="main__title">
+								Tenzies
+							</h2>
+							<p className="main__text">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+							<div className="main__game game">
+								<button className="game__item">1</button>
+								<button className="game__item">1</button>
+								<button className="game__item">1</button>
+								<button className="game__item">1</button>
+								<button className="game__item">1</button>
+								<button className="game__item">1</button>
+								<button className="game__item">1</button>
+								<button className="game__item">1</button>
+								<button className="game__item">1</button>
+								<button className="game__item game__item_active">1</button>
+							</div>
+							<button className="main__button">Roll</button>
 						</div>
-						<button className="main__button">Roll</button>
 					</div>
 				</div>
 			</section>
