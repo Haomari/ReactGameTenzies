632e5460bad94589ed0e9b89f9f6d16e3b71f042
Haomari <tcurpal@gmail.com>
2023-07-04 18:34:14 +0200

    refactor code, fixed some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-
 import Header from "./sections/Header";
 import Footer from "./sections/Footer";
 import Confetti from "react-confetti";
 
 function App() {
+  // State for storing the numbers
   const [numbers, setNumbers] = useState([
     { data: "", isSelect: false, id: 1 },
     { data: "", isSelect: false, id: 2 },
@@ -18,16 +18,21 @@ function App() {
     { data: "", isSelect: false, id: 10 },
   ]);
 
+  // State for game timer
   const [startTime, setStartTime] = useState(null);
   const [elapsedTime, setElapsedTime] = useState(0);
   const [timerRunning, setTimerRunning] = useState(false);
+
+  // State for game results
   const [gameResults, setGameResults] = useState([]);
 
+  // Function to start the game timer
   function startTimer() {
     setStartTime(Date.now());
     setTimerRunning(true);
   }
 
+  // Function to stop the game timer and save the result
   const stopTimer = () => {
     if (timerRunning) {
       const currentTime = Date.now();
@@ -36,7 +41,7 @@ function App() {
       setTimerRunning(false);
 
       const updatedResults = [...gameResults, elapsedTimeInMilliseconds];
-      // sort the game results from smallest to highest
+      // Sort the game results from smallest to highest
       updatedResults.sort((a, b) => a - b);
 
       setGameResults(updatedResults);
@@ -59,6 +64,7 @@ function App() {
     }
   }, [timerRunning, startTime]);
 
+  // Load game results from local storage
   useEffect(() => {
     const storedResults = localStorage.getItem("gameResults");
     if (storedResults) {
@@ -66,78 +72,66 @@ function App() {
     }
   }, []);
 
+  // Function to generate a random number
   function randomNumber() {
     return Math.floor(Math.random() * 9) + 1;
   }
 
-
-	function clearResults()  {
-    setGameResults([]);
-    localStorage.removeItem('gameResults');
-  };
-
-  function togleSelect(id) {
+  // Function to toggle the selection of a number
+  function toggleSelect(id) {
     setNumbers((prevNumbers) =>
-      prevNumbers.map((number) => {
-        return number.id === id && !allDataEmpty
+      prevNumbers.map((number) =>
+        number.id === id && !allDataEmpty && !hasWon
           ? { ...number, isSelect: !number.isSelect }
-          : number;
-      })
+          : number
+      )
     );
   }
 
+  // Function to roll the numbers
   function rollNumbers() {
     if (allDataEmpty) {
       startTimer();
       setNumbers((prevNumbers) =>
-        prevNumbers.map((number) => {
-          return number.isSelect ? number : { ...number, data: randomNumber() };
-        })
+        prevNumbers.map((number) =>
+          number.isSelect ? number : { ...number, data: randomNumber() }
+        )
       );
-    } else if (winNumbers()) {
-      stopTimer();
+    } else if (hasWon) {
+			stopTimer();
       setNumbers((prevNumbers) =>
-        prevNumbers.map((number) => {
-          return {
-            ...number,
-            data: "",
-            isSelect: !number.isSelect,
-          };
-        })
+        prevNumbers.map((number) => ({
+          ...number,
+          data: "",
+          isSelect: !number.isSelect,
+        }))
       );
     } else {
       setNumbers((prevNumbers) =>
-        prevNumbers.map((number) => {
-          return number.isSelect ? number : { ...number, data: randomNumber() };
-        })
+        prevNumbers.map((number) =>
+          number.isSelect ? number : { ...number, data: randomNumber() }
+        )
       );
     }
   }
 
+  // Check if all data in numbers array is empty
   const allDataEmpty = numbers.every((item) => !item.data);
 
+  // Check if the player has won the game
   function winNumbers() {
     const firstData = numbers[0].data;
     const allDataSame = numbers.every((item) => item.data === firstData);
-
     const allSelectTrue = numbers.every((item) => item.isSelect === true);
 
     return allDataSame && allSelectTrue;
   }
+	// Saves result in const
+	const hasWon = winNumbers();
 
-  const numbersToGame = numbers.map((number) => {
-    return (
-      <button
-        key={number.id}
-        className={
-          number.isSelect ? "game__item game__item_active" : "game__item"
-        }
-        onClick={() => togleSelect(number.id)}
-      >
-        {number.data}
-      </button>
-    );
-  });
+	if (hasWon) {
+		stopTimer()
+	}
 
   // Format the elapsed time into a string representation
   const formatElapsedTime = (time) => {
@@ -156,6 +150,19 @@ function App() {
     localStorage.removeItem("gameResults");
   }
 
+	  // Map numbers to game buttons
+		const numbersToGame = numbers.map((number) => (
+			<button
+				key={number.id}
+				className={
+					number.isSelect ? "game__item game__item_active" : "game__item"
+				}
+				onClick={() => toggleSelect(number.id)}
+			>
+				{number.data}
+			</button>
+		));
+
   // Render the game results list
   const gameResultsList = gameResults.map((result, index) => (
     <li key={index}>
@@ -167,7 +174,7 @@ function App() {
     <>
       <Header />
       <main className="page">
-        {winNumbers() && <Confetti />}
+        {hasWon && <Confetti />}
         <section className="page__main main">
           <div className="main__container">
             <div className="main__border">
@@ -182,16 +189,12 @@ function App() {
                 </div>
                 <div className="main__game game">{numbersToGame}</div>
                 <button onClick={rollNumbers} className="main__button">
-                  {winNumbers() ? "Finish" : allDataEmpty ? "Start" : "Roll"}
+                  {hasWon ? "Finish" : allDataEmpty ? "Start" : "Roll"}
                 </button>
-                <ul className="main__result">
-                  {gameResults.map((result, index) => (
-                    <li key={index}>
-                      Plase {index + 1}: {formatElapsedTime(result)}
-                    </li>
-                  ))}
-                </ul>
-								<button onClick={clearResults} className="main__reset">Reset</button>
+                <ul className="main__result">{gameResultsList}</ul>
+                <button onClick={clearResults} className="main__reset">
+                  Reset
+                </button>
               </div>
             </div>
           </div>
